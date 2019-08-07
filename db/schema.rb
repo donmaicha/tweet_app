4857352578545fae773a4bfaa4eaf56fb1f1d2c9
Hfukushima <genmaicha77783@gmail.com>
2019-08-07 14:16:28 +0900

    あってる？

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_15_070002) do
+ActiveRecord::Schema.define(version: 2019_08_06_140637) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
