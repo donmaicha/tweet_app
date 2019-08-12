6be9bcd2538b5602c9edc3f3719205ad8b1c075f
Hfukushima <genmaicha77783@gmail.com>
2019-08-12 18:12:38 +0900

    rails 学習コース6　終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-
-  post 'users/create' => 'users#create'
-  get 'signup' => 'users#new'
-
-  get 'users/index'
-  get 'users/:id' => 'users#show'
   
+  get '/' => "home#top"
+  get 'about' => "home#about"
+
+  # posts_routes
   get 'posts/index'
   get 'posts/new' => "posts#new"
   get 'posts/:id' => "posts#show"
@@ -14,7 +12,12 @@ Rails.application.routes.draw do
   post 'posts/:id/update' => 'posts#update'
   post 'posts/:id/destroy' => 'posts#destroy'
 
-  get '/' => "home#top"
-  get 'about' => "home#about"
+  # users_routes
+  post 'users/create' => 'users#create'
+  get 'signup' => 'users#new'
+  get 'users/index'
+  get 'users/:id' => 'users#show'
+  get 'users/:id/edit' => 'users#edit'
+  post 'users/:id/update' => 'users#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
