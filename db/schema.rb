f13cb37d55ab839fd39fe689bc87bf42cd6abd05
Hfukushima <genmaicha77783@gmail.com>
2019-08-12 23:17:38 +0900

    Ruby on Rails5 Ⅶ完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_140637) do
+ActiveRecord::Schema.define(version: 2019_08_12_092848) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -18,11 +18,7 @@ ActiveRecord::Schema.define(version: 2019_08_06_140637) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'stirng' for column 'image_name'
 
 end
